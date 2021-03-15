3f48a03aa2e2907f8a53c9f06d94d3b90243ee50
Simon Pilkington <simonpi@amazon.com>
2021-03-15 12:54:08 +1100

    Update Yams dependency, add SwiftSyntax dependency.

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 //
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2019-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License").
 // You may not use this file except in compliance with the License.
@@ -17,6 +17,9 @@ import PackageDescription
 
 let package = Package(
     name: "ServiceModelSwiftCodeGenerate",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v10)
+    ],
     products: [
         .library(
             name: "ServiceModelCodeGeneration",
@@ -33,25 +36,39 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/tachyonics/SwaggerParser.git", from: "0.6.4"), 
-        .package(url: "https://github.com/jpsim/Yams.git", from: "0.5.0")
+        .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.0"),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ServiceModelCodeGeneration",
-            dependencies: ["ServiceModelEntities"]),
+            name: "ServiceModelCodeGeneration", dependencies: [
+                .product(name: "SwiftSyntaxBuilder", package: "SwiftSyntax"),
+                .target(name: "ServiceModelEntities"),
+            ]
+        ),
         .target(
-            name: "ServiceModelEntities",
-            dependencies: []),
+            name: "ServiceModelEntities", dependencies: [
+            ]
+        ),
         .target(
-            name: "SwaggerServiceModel",
-            dependencies: ["ServiceModelCodeGeneration", "Yams", "SwaggerParser"]),
+            name: "SwaggerServiceModel", dependencies: [
+                .target(name: "ServiceModelCodeGeneration"),
+                .product(name: "Yams", package: "Yams"),
+                .product(name: "SwaggerParser", package: "SwaggerParser"),
+            ]
+        ),
         .target(
-            name: "ServiceModelGenerate",
-            dependencies: ["SwaggerServiceModel"]),
+            name: "ServiceModelGenerate", dependencies: [
+                .target(name: "SwaggerServiceModel"),
+            ]
+        ),
         .testTarget(
-            name: "ServiceModelEntitiesTests",
-            dependencies: ["ServiceModelEntities"]),
-    ]
+            name: "ServiceModelEntitiesTests", dependencies: [
+                .target(name: "ServiceModelEntities"),
+            ]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
