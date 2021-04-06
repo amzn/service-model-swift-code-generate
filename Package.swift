88371c148d2d582cbc11614f2c5d41ce6b93d498
Simon Pilkington <simonpi@amazon.com>
2021-04-06 12:01:08 +1000

    Remove swift-syntax dependency for now.

--- Package.swift
+++ Package.swift
@@ -37,14 +37,12 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/tachyonics/SwaggerParser.git", from: "0.6.4"), 
         .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.0"),
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ServiceModelCodeGeneration", dependencies: [
-                .product(name: "SwiftSyntaxBuilder", package: "SwiftSyntax"),
                 .target(name: "ServiceModelEntities"),
             ]
         ),
