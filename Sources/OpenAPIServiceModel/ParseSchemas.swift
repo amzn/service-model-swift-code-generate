7bcc2a8d5d6f98f5bab087f7735c19099e818c85
Mendez <mndzup@88665a076941.ant.amazon.com>
2021-07-28 14:06:43 -0700

    More print statements

--- Sources/OpenAPIServiceModel/ParseSchemas.swift
+++ Sources/OpenAPIServiceModel/ParseSchemas.swift
@@ -91,9 +91,8 @@ internal extension OpenAPIServiceModel {
             switch property {
             case .reference(let ref):
                 if let referenceName = ref.name {
-                    //print(referenceName)
-                    //print(name)
-                    print(objectContext.requiredProperties)
+                    print(name)
+                    print(objectContext.requiredProperties.contains(name))
                     structureDescription.members[name] = Member(value: referenceName, position: index,
                                                                 required: objectContext.requiredProperties.contains(name),
                                                                 documentation: nil)
